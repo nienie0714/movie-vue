132df13dfb50f98a2a8d42a48b4b4b50652b3f0d
niexinyu <niexinyu0714@163.com>
2018-08-07 17:50:05 +0800

    home

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <div class="content" :style="{background: 'url('+ dayforcast.img +')'}">
-      <!-- <img :src="dayforcast.img" alt=""> -->
+    <div class="content" :class="'weather-' + weatherIndex" v-if="this.daily_forecast.length">
       <div class="location">{{location}}</div>
       <div class="tmp">
         <div>
@@ -20,18 +19,20 @@
             </span>
         </div>
       </div>
+      <hr>
       <div class="date">{{dayforcast.date}}</div>
       <div class="forecast">
 
-        <div class="itemforecast" v-for="(item, index) in daily_forecast" :key="index" @click="getdayforeast(item, index)">
-          <div class="tmp_text">{{item.cond_txt_d}}</div>
+        <div data-sup="{a:1}" class="itemforecast" v-for="(item, index) in daily_forecast" :key="index" @click="switchWeather(item.cond_txt_d, index)">
+          <div class="img" :class="'weather-logo-' + weathers.indexOf(item.cond_txt_d)"></div>
+          <div class="tmp-text">{{item.cond_txt_d}}</div>
           <span class="tmp_min">
             {{item.tmp_min}}℃
           </span>~<span class="tmp_max">
             {{item.tmp_max}}℃
             </span>
         </div>
-        <div class="hr"></div>
+        <div class="bord"></div>
       </div>
     </div>
   </div>
@@ -49,54 +50,73 @@
       return {
         location: '',
         daily_forecast: [],
-        dayforcast: {}
+        weathers: ['晴', '小雨', '阵雨', '雷阵雨', '多云', '扬沙', '雪'],
+        weatherIndex: 0,
+        daylyIndex: 0
       };
     },
     created() {
       this.getforecast();
       this.getHourly();
     },
-    computed: {},
+    computed: {
+      dayforcast() {
+        return this.daily_forecast.length && this.daily_forecast[this.daylyIndex];
+      }
+    },
     watch: {},
     methods: {
       getforecast() {
         let data = {
           key: 'c827b4ed3a9445b480048b70f09ade74',
-          location: 'beijing'
+          location: 'xian'
         };
         htp.get(url.forecast, data).then(res => {
           res = res.data.HeWeather6[0];
           if(res.status === "ok") {
             this.location = res.basic.location;
             this.daily_forecast = res.daily_forecast;
-            this.getdayforeast(this.daily_forecast[0]);
+            this.switchWeather(this.daily_forecast[0].cond_txt_d);
           }
         });
       },
-      getdayforeast(item) {
-        this.dayforcast = item;
-        let weather = item.cond_txt_d;
-        switch(weather) {
-          case "晴":
-            this.dayforcast.img = require("../assets/sun1.jpg");
-            break;
-          case "雨":
-            this.dayforcast.img = require("../assets/rain.jpg");
-            break;
-          case "阵雨":
-          case "雷阵雨":
-            this.dayforcast.img = require("../assets/zhenyu.jpg");
-            break;
-          case "多云":
-            this.dayforcast.img = require("../assets/cloudy.jpg");
-            break;
-          case "雪":
-            this.dayforcast.img = require("../assets/snow.jpg");
-            break;
-          default:
-            this.dayforcast.img = require("../assets/sun.jpg");
-        }
+      switchWeather(name, index = 0) {
+        this.weatherIndex = this.weathers.indexOf(name);
+        this.daylyIndex = index;
       },
+      // getdayforeast(item) {
+      //   this.dayforcast = item;
+      //   let weather = item.cond_txt_d;
+      //   switch(weather) {
+      //     case "晴":
+      //       this.dayforcast.img = require("../assets/sun1.jpg");
+      //       this.dayforcast.simg = require("../assets/ssun.png");
+      //       break;
+      //     case "雨":
+      //       this.dayforcast.img = require("../assets/rain.jpg");
+      //       this.dayforcast.simg = require("../assets/srain.png");
+      //       break;
+      //     case "阵雨":
+      //     case "雷阵雨":
+      //       this.dayforcast.img = require("../assets/zhenyu.jpg");
+      //       this.dayforcast.simg = require("../assets/szhen.png");
+      //       break;
+      //     case "多云":
+      //       this.dayforcast.img = require("../assets/cloudy.jpg");
+      //       this.dayforcast.simg = require("../assets/scloudy.png");
+      //       break;
+      //     case "扬沙":
+      //       this.dayforcast.img = require("../assets/sand.jpg");
+      //       this.dayforcast.simg = require("../assets/ssha.png");
+      //       break;
+      //     case "雪":
+      //       this.dayforcast.img = require("../assets/snow.jpg");
+      //       this.dayforcast.simg = require("../assets/ssnow.png");
+      //       break;
+      //     default:
+      //       this.dayforcast.img = require("../assets/sun.jpg");
+      //   }
+      // },
       getHourly() {
         let data = {
           key: 'c827b4ed3a9445b480048b70f09ade74',
@@ -115,29 +135,55 @@
     padding: 15px 30px;
     color: #fff;
     // background: linear-gradient(#b5effa, #5d88c1);
-    background-repeat: no-repeat;
-    background-position: center center;
-    background-size: 100% 100%;
-    text-align: left;
+    background: ~'no-repeat 0 0 / 100% 100%';
     .location {
-      padding: 50px 0 0 0;
+      padding-top: 50px;
       font-size: 48px;
     }
+
+    &.weather-0 {
+      background-image: url('../assets/sun.jpg');
+    }
+    &.weather-1 {
+      background-image: url('../assets/rain.jpg');
+    }
+    &.weather-2,
+    &.weather-3 {
+      background-image: url('../assets/zhenyu.jpg');
+    }
+    &.weather-4 {
+      background-image: url('../assets/cloudy.jpg');
+    }
+    &.weather-5 {
+      background-image: url('../assets/sand.jpg');
+    }
+    &.weather-6 {
+      background-image: url('../assets/snow.jpg');
+    }
     .tmp {
-      padding: 30px 0 0 0;
+      padding-top: 30px;
       .num {
         font: bold 120px 'Minion Pro';
       }
       .text {
         font-size: 36px;
       }
+      .temrange {
+        // width: 250px;
+        // border-bottom: 4px solid rgba(255, 255, 255, 0.6);
+        padding-bottom: 15px;
+      }
       span {
         font-size: 30px;
       }
     }
+    hr {
+      width: 40%;
+      opacity: 0.6;
+    }
     .date {
       font-size: 18px;
-      padding: 30px 0 0 0;
+      padding-top: 30px;
     }
     .forecast {
       display: flex;
@@ -149,15 +195,36 @@
       bottom: 0;
       left: 0;
       .itemforecast {
-        .tmp_text {
+        .img {
+          width: 96px;
+          height: 96px;
+          background: ~'no-repeat 0 0 / 100% 100%';
+          margin: 0 auto;
+          &.weather-logo-0 {
+            background-image: url('../assets/ssun.png');
+          }
+          &.weather-logo-1 {
+            background-image: url('../assets/srain.png');
+          }
+          &.weather-logo-2,
+          &.weather-logo-3 {
+            background-image: url('../assets/szhen.png');
+          }
+          &.weather-logo-4 {
+            background-image: url('../assets/scloudy.png');
+          }
+          &.weather-logo-5 {
+            background-image: url('../assets/ssnow.png');
+          }
+          &.weather-logo-6 {
+            background-image: url('../assets/sun.jpg');
+          }
+        }
+        .tmp-text {
           font-size: 30px;
         }
-        display: inline-block;
         width: 33.3%;
         text-align: center;
-      }
-      .hr {
-        border: 2px;
       }
     }
   }
